d4db102819df12df593a9f5d69e4dd9cd88f7f5b
BLoushi52 <k.albloushii@hotmail.com>
2022-10-23 19:39:03 +0300

    created users and planner apps and a home page

--- event_planner/urls.py
+++ event_planner/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from planner.views import get_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", get_home, name="home" ), 
 ]
