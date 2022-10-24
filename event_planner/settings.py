2eb44025babdba26a452fdb17fad01ce95721137
Khaled Alghanim <al.ghanim@me.com>
2022-10-24 10:17:52 +0300

    Prepared static and media files and installed Pillow

--- event_planner/settings.py
+++ event_planner/settings.py
@@ -118,9 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- event_planner/urls.py
+++ event_planner/urls.py
@@ -17,9 +17,15 @@ from django.contrib import admin
 from django.urls import path
 from planner.views import get_home
 from users.views import register_user
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", get_home, name="home" ), 
     path("register/", register_user, name="register" ), 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
