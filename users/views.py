2dc3044803d06c5c00cbd39e592e8e8b2099a955
Khaled Alghanim <al.ghanim@me.com>
2022-10-25 10:40:55 +0300

    Passed and next events is showing based on the user (Staff users, or Customer)

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
 import os
+import datetime
+from django.utils import timezone
 from django.shortcuts import render,redirect
 from users.forms import RegistrationForm, LoginForm
 from django.contrib.auth import login, logout, login, authenticate
@@ -38,22 +40,55 @@ def login_user(request):
             auth_user = authenticate(username=username, password=password)
             if auth_user is not None:
                 login(request, auth_user)
-                return redirect("home")
+                return redirect("events-list")
     context = {"form": form}
     return render (request, "login.html", context)
 
 def get_events(request):
     events_list = Event.objects.all()
     path = settings.MEDIA_ROOT
-    
+
+
     new_list = []
     for event in events_list:
+
+        today = datetime.date(timezone.now().year,timezone.now().month,timezone.now().day)
+        event_date = datetime.date(event.date.year,event.date.month,event.date.day)
+
+        days_different = event_date - today
+
+        days_message = ""
+        passed = False
+
+        if days_different.days >= 0 :
+            days_message = f"{days_different.days} days to go."
+        else:
+            days_message = f"Passed!"
+            passed = True
+
         img_list = (f"{path}/{event.image}")
-        new_list.append({
-            "name": event.name,
-            "image": img_list,
-            "date": event.date,
-        })
+        if request.user.is_staff:
+            new_list.append({
+                "name": event.name,
+                "image": img_list,
+                "date": event.date,
+                "available_seats": (event.number_of_seats - event.number_of_booked_seats),
+                "days_to_go": days_message ,
+                "passed": passed,
+            })
+        else:
+            if not passed:
+                new_list.append({
+                    "name": event.name,
+                    "image": img_list,
+                    "date": event.date,
+                    "available_seats": (event.number_of_seats - event.number_of_booked_seats),
+                    "days_to_go": days_message ,
+                    "passed": passed,
+                })
+
     context = {"events": new_list}
-    print(context)
+
+
     return render (request, "events_list.html", context)
+    
